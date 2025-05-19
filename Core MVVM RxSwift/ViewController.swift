58496005835949919ab23a454dc79bd0bd6651eb
Stefan Jivalino <stefan.jivalino@transvision.co.id>
2025-05-19 13:43:41 +0700

    Initial Commit

---
+++ Core MVVM RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core MVVM RxSwift
+//
+//  Created by Stefan Jivalino on 19/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
